32006e277b2fd62de224eec4393045910eb08806
darlingson <dmakuwila@gmail.com>
2023-12-08 20:58:50 +0200

    added the bottom navigation item

--- app/src/main/java/com/codeshinobi/mingoli_music_player/Screens.kt
+++ app/src/main/java/com/codeshinobi/mingoli_music_player/Screens.kt
@@ -2,6 +2,6 @@ package com.codeshinobi.mingoli_music_player
 
 sealed class Screens(val route : String) {
     object Home : Screens("home_screen")
-    object Search : Screens("library_screen")
-    object Profile : Screens("settings_screen")
+    object Library : Screens("library_screen")
+    object Settings : Screens("settings_screen")
 }
